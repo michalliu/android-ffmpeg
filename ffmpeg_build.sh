16792c2541f1b9babb4ddb3d0f27b51366499572
michalliu <michalliu@foxmail.com>
2015-10-15 22:15:39 +0800

    strip down ffmpeg

--- ffmpeg_build.sh
+++ ffmpeg_build.sh
@@ -4,12 +4,21 @@
 
 pushd ffmpeg
 
+CFLAGS="$CFLAGS -O3 -DANDROID"
+
 case $1 in
   armeabi)
-    CPU='cortex-a5'
+    CPU="cortex-a5"
+    ABLES="--disable-neon"
   ;;
-  armeabi-v7a | armeabi-v7a-neon)
-    CPU='cortex-a8'
+  armeabi-v7a)
+    CFLAGS="$CFLAGS -march=armv7-a -mfloat-abi=softfp"
+    ABLES="--enable-asm --enable-thumb --enable-hwaccels --cpu=armv7-a"
+    CPU="cortex-a8"
+  ;;
+  armeabi-v7a-neon)
+    CFLAGS="$CFLAGS -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
+    ABLES="--enable-asm --enable-thumb --enable-hwaccels --cpu=armv7-a --enable-neon"
   ;;
   x86)
     CPU='i686'
@@ -18,6 +27,7 @@ esac
 
 make clean
 
+config_standard() {
 ./configure \
 --target-os="$TARGET_OS" \
 --cross-prefix="$CROSS_PREFIX" \
@@ -25,6 +35,7 @@ make clean
 --cpu="$CPU" \
 --enable-runtime-cpudetect \
 --sysroot="$NDK_SYSROOT" \
+$ABLES \
 --enable-pic \
 --enable-libx264 \
 --enable-libass \
@@ -49,6 +60,55 @@ make clean
 --extra-ldflags="-L${TOOLCHAIN_PREFIX}/lib $LDFLAGS" \
 --extra-libs="-lpng -lexpat -lm" \
 --extra-cxxflags="$CXX_FLAGS" || exit 1
+}
+
+#strip down ffmpeg
+config_small(){
+./configure \
+--target-os="$TARGET_OS" \
+--cross-prefix="$CROSS_PREFIX" \
+--arch="$NDK_ABI" \
+--cpu="$CPU" \
+--enable-runtime-cpudetect \
+--sysroot="$NDK_SYSROOT" \
+$ABLES \
+--disable-debug \
+--disable-doc \
+--disable-ffplay \
+--disable-ffserver \
+--disable-stripping \
+--disable-avdevice \
+--disable-postproc \
+--disable-network \
+--enable-pthreads \
+--enable-small \
+--disable-encoders \
+--enable-encoder=libx264 \
+--enable-encoder=libfaac \
+--enable-libx264 \
+--enable-libfaac \
+--disable-decoders \
+--disable-parsers \
+--disable-muxers \
+--enable-muxer=mp4 \
+--disable-demuxers \
+--disable-protocols \
+--disable-devices \
+--disable-bsfs \
+--enable-nonfree \
+--enable-static \
+--enable-gpl \
+--enable-pic \
+--pkg-config="${2}/ffmpeg-pkg-config" \
+--prefix="${2}/build/${1}" \
+--extra-cflags="-I${TOOLCHAIN_PREFIX}/include $CFLAGS" \
+--extra-ldflags="-L${TOOLCHAIN_PREFIX}/lib $LDFLAGS" \
+--extra-libs="-lpng -lexpat -lm" \
+--extra-cxxflags="$CXX_FLAGS" || exit 1
+}
+
+#config_standard $@
+config_small $@
 
 make V=s -j${NUMBER_OF_CORES} && make install || exit 1
 
